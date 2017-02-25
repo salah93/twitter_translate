f3649c081870c4db6ca533d6df33c9aa4b1b3e5e
salah ahmed <salah93@crosscompute.com>
2017-02-24 21:27:22 -0500

    take out debugging code

--- translate_bot.py
+++ translate_bot.py
@@ -33,7 +33,7 @@ def reply_to_user(tweet_id, status):
     url = 'https://api.twitter.com/1.1/statuses/update.json'
     body = 'in_reply_to_status_id=%s&status=%s' % (tweet_id, status)
     resp, content = client.request(url, method='POST', body=body, headers=None)
-    from IPython import embed; embed()
+    return content
 
 
 def get_text_and_user(content):
@@ -70,10 +70,11 @@ if __name__ == '__main__':
     access_token = os.environ['TWITTER_ACCESS_TOKEN']
     access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
     client = oauth_req(consumer_key, consumer_secret, access_token, access_token_secret)
-    content = json.loads(search_hashtag('#translate_salah'))
-    since_id = content['search_metadata']['max_id_str']
-    get_text_and_user(content)
+    # get past since_id
+    since_id = '0'
     content = json.loads(search_hashtag('#translate_salah', since_id))
+    # update since_id
+    since_id = content['search_metadata']['max_id_str']
     results = get_text_and_user(content)
     for i in results:
         print("user: %s\ntweet: %s\n\n" % (i[0], i[1]))
